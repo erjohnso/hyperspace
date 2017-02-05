c05be0e422aa3437c3c399925f5aab3fde520f52
Eric Johnson <erjohnso@google.com>
2017-02-05 13:52:30 +0000

    systemd fix

--- packer/scripts/hyperspace.sh
+++ packer/scripts/hyperspace.sh
@@ -33,6 +33,9 @@ sudo -u hyperspace sed -i '28,33d' etc/nginx.conf
 # install nginx
 sudo apt-get install -y nginx
 sudo rm /etc/nginx/sites-enabled/default
-sudo cp /srv/hyperspace/hyperspace/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -s /srv/hyperspace/hyperspace/etc/hyperspace.service /etc/systemd/system/
+sudo ln -s /srv/hyperspace/hyperspace/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -s /srv/hyperspace/hyperspace/etc/hyperspace.service /etc/systemd/system/multi-user.target.wants/
 
+# stop/start services
+#sudo systemctl restart hyperspace
+#sudo systemctl restart nginx
